e5334872148b584f1298ae5c49383b5f3225363f
Fernando Braga <bragafernando317@gmail.com>
2024-08-27 21:27:28 -0300

    Adding first test case to user-register flow

---
+++ QAzando.Framework/QAzando.Framework/Pages/HomePage.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using QAzando.Framework.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QAzando.Framework.Pages
+{
+    public class HomePage : BaseSettings
+    {
+        public HomePage(IWebDriver driver) : base(driver) { }
+
+        public void ClickLoginIcon()
+        {
+            Driver.FindElement(By.CssSelector(".fa.fa-lock")).Click();
+        }
+
+        public void EnterUserCredentials()
+        {
+            DataGenerator.GenerateUserData();
+
+            Driver.FindElement(By.Id("user")).SendKeys(DataGenerator.FirstName);
+            Driver.FindElement(By.Id("email")).SendKeys(DataGenerator.Email);
+            Driver.FindElement(By.Id("password")).SendKeys(DataGenerator.Password);           
+        }
+
+        public void SubmitForm()
+        {
+            Driver.FindElement(By.Id("btnRegister")).Click();
+        }
+
+        public void VerifyWelcomeMessage()
+        {
+            string welcomeMessage = Driver.FindElement(By.Id("swal2-html-container")).Text;
+            string expectedMessage = $"Bem-vindo {DataGenerator.FirstName}";
+            Assert.That(welcomeMessage, Does.Contain(expectedMessage));
+        }
+
+    }
+}
